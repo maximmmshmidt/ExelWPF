0a24efc41e3c2eb178fa4f7dea819effa3928be0
Platon <hypek60@gmail.com>
2023-01-26 15:02:06 +0500

    сделал вывод данных в word

--- WpfApp1/Pages/ProfilStudentPage.xaml.cs
+++ WpfApp1/Pages/ProfilStudentPage.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WpfApp1.Models;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace WpfApp1.Pages
 {
@@ -22,18 +15,33 @@ namespace WpfApp1.Pages
     public partial class ProfilStudentPage : Page
     {
         public List<Journals> mass;
+        Students currentStudent;
         public ProfilStudentPage(Students activeStudent)
         {
             InitializeComponent();
             // Информация о студенте 
-            FIOTextBlock.Text = activeStudent.LastName + " " + activeStudent.FiestName + " " + activeStudent.PatronomicName;
+            FIOTextBlock.Text = activeStudent.FiestName + " " + activeStudent.LastName + " " + activeStudent.PatronomicName;
             ProfesiaTextBlock.Text = activeStudent.Professions.NameProfession;
             GroupsTextBlock.Text = activeStudent.Groups.NameGroup;
             FormTrainingTextBlock.Text = activeStudent.FormTime.Name;
             YearTrainingTextBlock.Text = activeStudent.YearAdd.Year.ToString();
-            //
+            currentStudent = activeStudent;
             mass = activeStudent.Journals.ToList(); 
             DataGridJournals.ItemsSource = mass;
+        }
+        private void DiplomButtonClick(object sender, RoutedEventArgs e)
+        {
+            Word.Application application = new Word.Application();
+            string file = $"{Directory.GetCurrentDirectory()}\\Docs\\Диплом.doc";
+            if (File.Exists(file))
+            {
+                Word.Document doc = application.Documents.Open(file);
+                doc.Activate();
+                doc.Bookmarks["FIO"].Range.Text = FIOTextBlock.Text;
+                doc.Bookmarks["Profession"].Range.Text = ProfesiaTextBlock.Text;
+                application.Visible = true;
+                doc.SaveAs($"{Directory.GetCurrentDirectory()}\\Docs\\{currentStudent.FiestName}Диплом.doc");
+            }
         }
     }
 }
